0619aa97dba19bf452cac6da1b5f3e826115253c
ayelen3976 <ayee_01@live.com>
2020-09-08 01:35:00 -0300

    day 1

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -10,10 +10,10 @@ class Resume extends Component {
         <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
         <p>{education.description}</p></div>
       })
-      var work = this.props.data.work.map(function(work){
-        return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
+      var ITstudy = this.props.data.ITstudy.map(function(ITstudy){
+        return <div key={ITstudy.company}><h3>{ITstudy.company}</h3>
+            <p className="info">{ITstudy.title}<span>&bull;</span> <em className="date">{ITstudy.years}</em></p>
+            <p>{ITstudy.description}</p>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
@@ -40,14 +40,14 @@ class Resume extends Component {
       </div>
 
 
-      <div className="row work">
+      <div className="row ITstudy">
 
          <div className="three columns header-col">
-            <h1><span>Work</span></h1>
+            <h1><span>study IT</span></h1>
          </div>
 
          <div className="nine columns main-col">
-          {work}
+          {ITstudy}
         </div>
     </div>
 
